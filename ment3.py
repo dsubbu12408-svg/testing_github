02f966522c20836996f21366ee232508eea9ec4e
unknown <dsubbu12408@gmail.comgit config --global user.name>
2026-04-08 23:44:53 +0530

    adding new ment3 file

---
+++ ment3.py
@@ -0,0 +1,9 @@
+age = 25
+age_months = age * 12
+age_days = age * 365
+remainder = age % 7
+age_squared = age ** 2
+print(f"Age in months: {age_months}")
+print(f"Age in days: {age_days}")
+print(f"Remainder when divided by 7: {remainder}")
+print(f"Age squared: {age_squared}")
